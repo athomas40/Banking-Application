dee803b46ec4086b3952377f133f2fc463b00cc8
Stephanie Yung <Stephanie.Yung43@myhunter.cuny.edu>
2021-04-12 19:08:47 -0400

    installed axios

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
 import LogIn from './components/LogIn';
+import axios from "axios";
 
 class App extends Component {
   constructor(){
