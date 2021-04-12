c5a4b79fdd9530af906b41f2e676eee0d9166bf1
Stephanie Yung <Stephanie.Yung43@myhunter.cuny.edu>
2021-04-12 19:14:12 -0400

    account balance with apis

--- src/App.js
+++ src/App.js
@@ -9,14 +9,42 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      accountBalance: 14500.50,
+      accountBalance: 0,
       currentUser:{
         userName: 'amy_smith',
         memberSince: '01/14/00',
-      }
+      },
+      debits: [],
+      credits: []
     }
+    // this.addDebit = this.addDebit.bind(this)
+    // this.addCredit = this.addCredit.bind(this)
 
   }
+
+  async componentDidMount(){
+    let debits = await axios.get("https://moj-api.herokuapp.com/debits");
+    let credits = await axios.get("https://moj-api.herokuapp.com/credits");
+
+    //get data from API response
+    debits = debits.data
+    credits = credits.data
+
+    let debitSum = 0, creditSum = 0;
+    debits.forEach((debit) =>{
+      debitSum += debit.amount
+    })
+
+    credits.forEach((credit) => {
+      creditSum += credit.amount
+    })
+
+    const accountBalance = creditSum - debitSum;
+
+    this.setState({debits, credits, accountBalance});
+  }
+
+
   mockLogIn = (logInInfo) => {
     const newUser = {...this.state.currentUser}
     newUser.userName = logInInfo.userName
