9832de7231e87726762a4e2ee1d043b01830cb55
Aleena Thomas <aleenathomas@Aleenas-MBP.lan>
2021-04-14 18:44:08 -0400

    stores new debit added to the debits display

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -1,13 +1,58 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import AccountBalance from './AccountBalance';
 
 class Debits extends Component{
     constructor(props){
         super(props)
-        this.state ={
-            debits: this.props.allDebits,
+        this.state = {
+            description: '',
+            amount: 0,
+            date: '',
+
+            alldebits: [],
+            alldescription: [],
+            allamount: [],
+            alldate: [],
         }
+    }
+    componentDidMount() {
+        let descipt = [], money = [], day = [], currentdebit = [], tempdebit = [];
+        //stores debit values from API
+        for (let i=0; i< this.props.allDebits.length; i++)  {
+            descipt.push(this.props.allDebits[i].description);
+            money.push(this.props.allDebits[i].amount);
+            day.push(this.props.allDebits[i].date);
+
+            tempdebit = [];
+            tempdebit.push(this.props.allDebits[i].description, this.props.allDebits[i].amount, this.props.allDebits[i].date);
+            currentdebit.push(tempdebit);
+        }
+        this.setState({
+            alldescription: descipt,
+            allamount: money,
+            alldate: day,
+            alldebits: currentdebit,
+        })
+    }
+
+    submitDebits = (event) => {
+        event.preventDefault();
+        let formattedDate = new Date(this.state.date).toISOString();    //formats date as UTC
+        let tempdebit = [this.state.description, this.state.amount, formattedDate];
+        let currentdebit = this.state.alldebits;
+        currentdebit.push(tempdebit);
+        
+        this.setState({alldebits: currentdebit});
+    }
+
+    handleChangeDescription = (des) => {
+        this.setState({description: des.target.value });
+    }
+    handleChangeAmount = (num) => {
+        this.setState({amount: num.target.value});
+    }
+    handleChangeDate = (day) => {
+        this.setState({date: day.target.value});
     }
 
     render(){
@@ -15,24 +60,25 @@ class Debits extends Component{
             <div>
                 <h1>Debits</h1>
                 {
-                    this.props.allDebits.map( (debit) => {
+                    this.state.alldebits.map( (debits, index) => {
                         return (
                             <div>
-                                <li>{debit.amount} - {debit.description} - {debit.date}</li>
+                                <li key={index}>${debits[1]} {debits[0]} {debits[2]}</li>
                             </div>
                         )
                     })
                 }
+                
                 <div>
                 <br></br>
-                <form>
+                <form onSubmit={this.submitDebits}>
                     <label>
                         Description:
-                        <input type="text" name="description" />
+                        <input type="text" name="description" onChange={this.handleChangeDescription}/>
                         Amount:
-                        <input type="number" name="amount" />
+                        <input type="number" name="amount" onChange={this.handleChangeAmount}/>
                         Date:
-                        <input type="date" name="date" />
+                        <input type="date" name="date" min="2000-01-01" max="2022-12-31" placeholder="yyyy-mm-dd" onChange={this.handleChangeDate}/>
                     </label>
                     <input type="submit" value="Submit" />
                 </form>
